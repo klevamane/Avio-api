eb75a76932095d7962f654a524c8f71b1a7cb115
Onengiye Onengiye <klevamane@gmail.com>
2021-04-13 02:07:38 +0100

    add product controller and refactor the product routes

---
+++ controllers/product.controller.js
@@ -0,0 +1,34 @@
+import Product from '../models/product.js';
+import asyncHandler from 'express-async-handler';
+
+/**
+ * Returns all products
+ * @function getProducts
+ * @param  {object} req  The request object.
+ * @param  {object} res  The response object
+ * @param  {String} next Moves the operation to the next handler or middleware
+ * @return {object}
+ */
+const getProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find({});
+  res.json({ products });
+});
+
+/**
+ * Returns a single product by it's id
+ * @function getProducts
+ * @param  {object} req  The request object.
+ * @param  {object} res  The response object
+ * @param  {String} next Moves the operation to the next handler or middleware
+ * @return {object}
+ */
+const getProductById = asyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    return res.json(product);
+  }
+  res.status(404);
+  throw new Error('Product not found');
+});
+
+export { getProducts, getProductById };
--- routes/product.js
+++ routes/product.js
@@ -1,27 +1,13 @@
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/product.controller.js';
+
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-import Product from '../models/product.js';
 
 const router = express.Router();
 
-router.get(
-  '/',
-  asyncHandler(async (req, res, next) => {
-    const products = await Product.find({});
-    res.json({ products });
-  }),
-);
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.json(product);
-    }
-    res.status(404);
-    throw new Error('Product not found');
-  }),
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
