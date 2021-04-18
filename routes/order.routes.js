8fb119d4e5283f550282c5dc0a0538d589c53b88
Onengiye Onengiye <klevamane@gmail.com>
2021-04-18 12:16:36 +0100

    add feature to get an order by Id

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,9 +1,14 @@
-import { addOrderItems } from '../controllers/order.controller.js';
+import {
+  addOrderItems,
+  getOrderById,
+} from '../controllers/order.controller.js';
+
 import express from 'express';
 import { protect } from '../middleware/auth.middleware.js';
 
 const orderRouter = express.Router();
 
 orderRouter.route('/').post(protect, addOrderItems);
+orderRouter.route('/:id').get(protect, getOrderById);
 
 export default orderRouter;
